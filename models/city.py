774f6691daac2a01ab23308c31975c8097039858
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2024-03-20 18:09:04 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
+class City(BaseModel, Base):
+    """ Model representation of `citiy` table
+    Attributes:
+        state_id: The state id
+        name: input name
+        places: constraint to delete a Place instance if its
+                corresponding City instance is deleted.
+    """
     __tablename__ = "cities"
-    name = Column(String(100), nullable=False)
-    state_id = Column(String(50), ForeignKey('states.id'), nullable=False)
 
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place",
+                              cascade='all, delete, delete-orphan',
+                              backref="cities")
+    else:
+        state_id = ''
+        name = ''
