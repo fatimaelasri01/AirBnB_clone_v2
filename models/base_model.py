9e6a62edf385ccdb68c68e8faad0014eadd735c9
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2024-03-20 18:00:34 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,20 @@ class BaseModel:
                     DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
+        """Instantiates a new model
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -48,7 +61,7 @@ class BaseModel:
             return '[{}] ({}) {}'.format(cls, self.id, new_dict)
         else:
             return '[{}] ({}) {}'.format(cls, self.id, new_dict)
-
+            
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
@@ -58,10 +71,6 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        # dictionary = {}
-        # dictionary.update(self.__dict__)
-        # dictionary.update({'__class__':
-        #                  (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary = dict(self.__dict__)
         dictionary["__class__"] = str(type(self).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
