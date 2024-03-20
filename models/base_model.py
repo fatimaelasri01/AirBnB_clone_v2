6ef8b865712aafe37c80f58aaee5e0254a01416a
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2024-03-20 18:02:39 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,88 +1,21 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import uuid
-from datetime import datetime
+""" City Module for HBNB project """
+from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
-
-Base = declarative_base()
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(
-                    DateTime, default=datetime.utcnow(), nullable=False)
-    updated_at = Column(
-                    DateTime, default=datetime.utcnow(), nullable=False)
+class City(BaseModel, Base):
+    """ Model representation of `citiy` table"""
+    __tablename__ = "cities"
 
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new model
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-                if 'id' not in kwargs:
-                    self.id = str(uuid.uuid4())
-                if 'created_at' not in kwargs:
-                    self.created_at = datetime.now()
-
-                if 'created_at' in kwargs and 'updated_at' not in kwargs:
-                    self.updated_at = self.created_at
-                else:
-                    self.updated_at = datetime.now()
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        new_dict = self.__dict__
-        if getenv('HBNB_TYPE_STORAGE') == 'db':
-            if new_dict['_sa_instance_state']:
-                del new_dict['_sa_instance_state']
-            return '[{}] ({}) {}'.format(cls, self.id, new_dict)
-        else:
-            return '[{}] ({}) {}'.format(cls, self.id, new_dict)
-            
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()  # Take note, might need to change.
-        storage.new(self)
-        storage.save()
-
-    def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = dict(self.__dict__)
-        dictionary["__class__"] = str(type(self).__name__)
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        try:
-            if dictionary['_sa_instance_state']:
-                dictionary.pop('_sa_instance_state')
-            return dictionary
-        except KeyError:
-            pass
-
-    def delete(self):
-        """delete the current instance from the storage"""
-        from models import storage
-        storage.delete(self)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place",
+                              cascade='all, delete, delete-orphan',
+                              backref="cities")
+    else:
+        state_id = ''
+        name = ''
