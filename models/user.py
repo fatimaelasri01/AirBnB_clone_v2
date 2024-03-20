a5db35240cd908a7e9c756884f9b3bc5c1932362
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2024-03-20 17:50:54 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+"""This module uniquely instantiates
+a storage for the application"""
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from os import getenv
 
+if getenv('HBNB_TYPE_STORAGE') == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
-class User(BaseModel, Base):
-    """a User class containing user inf"""
-    __tablename__ = 'users'
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+storage.reload()
