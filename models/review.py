26781ff51ea7fe80719ad25f2fdc8588f835809c
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2024-03-21 12:07:01 +0000

    Update review.py

--- models/review.py
+++ models/review.py
@@ -2,12 +2,12 @@
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
-    """The Review class containing text, place_id, user_id"""
+    """ Model representation of `review` table """
     __tablename__ = "reviews"
+    
     text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
