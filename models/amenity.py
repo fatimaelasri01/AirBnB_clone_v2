1c1f6819412ac112a9a584e949579306f98c822f
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2024-03-21 12:08:53 +0000

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table
+from os import getenv
+from sqlalchemy import Table, Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place, place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """The Amenity class containing name"""
+    """
+    Python model representation of `amenities` in database """
     __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity,
+                                       back_populates='amenities')
+    else:
+        name = ''
