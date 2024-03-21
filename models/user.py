4177c8deed0d48e81d00a06585baaa9ec0317409
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2024-03-21 12:02:12 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -3,15 +3,26 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
-    __tablename__ = 'users'
+    """Python model representation of a `User` table """
+    __tablename__ = "users"
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    places = relationship("Place", cascade="all, delete", backref="user")
-    reviews = relationship("Review", cascade="all, delete", backref="user")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        places = relationship("Place",
+                              cascade="all, delete, delete-orphan",
+                              backref="user")
+        reviews = relationship("Review",
+                               cascade="all, delete, delete-orphan",
+                               backref="user")
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
