5c52c7da5360dea0cdd5a2f9478b3984273c3192
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-04-08 02:11:24 +0000

    up

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,42 +1,27 @@
 #!/usr/bin/env bash
+# sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-#check if script is run as root
-if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run as root" 1>&2
-    exit 1
-fi
-
-#install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    apt-get -y update
-    apt-get -y install nginx
-fi
-
-#create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-
-#create fake HTML file
-web="<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>"
-echo "$web" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-#create symbolic link
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-#give ownership of /data/ to ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-#update Nginx configuration
-sed -i '/hbnb_static/ { /alias/ { s/#//; } }' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-#restart Nginx
-service nginx restart
-
-exit 0
-
+sudo service nginx restart
